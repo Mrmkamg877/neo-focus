150a0d90ca90ca1fa039cd7c054019d9864c9673
Mrmkamg877 <Mansour877+1@gmail.com>
2025-03-26 06:59:41 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,32 @@
-const CACHE_NAME = 'neo-focus-v3';
+const CACHE_NAME = 'neo-focus-v4';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.webmanifest',
-  '/icon-192-maskable.png',
-  '/icon-512-any.png',
-  '/icon-144-any.png',
-  '/screenshot1.png'
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192-maskable.png',
+  './icon-512-any.png',
+  './icon-144-any.png',
+  './screenshot1.png'
 ];
 
-// Install event
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
+      .then(cache => {
+        console.log('Caching assets');
+        return cache.addAll(ASSETS);
+      })
       .then(() => self.skipWaiting())
   );
 });
 
-// Activate event
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cache => {
           if (cache !== CACHE_NAME) {
+            console.log('Deleting old cache:', cache);
             return caches.delete(cache);
           }
         })
@@ -33,10 +35,11 @@ self.addEventListener('activate', (event) => {
   );
 });
 
-// Fetch event
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
