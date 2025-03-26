2000a255ccd594b7a2a2a7eeb556f346e0f6ede4
Mrmkamg877 <Mansour877+1@gmail.com>
2025-03-26 05:53:59 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,21 +2,19 @@ const CACHE_NAME = 'neo-focus-v1';
 const ASSETS = [
   '/',
   '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  '/manifest.webmanifest'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
+      .then((cache) => cache.addAll(ASSETS))
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then((response) => response || fetch(event.request))
   );
 });
